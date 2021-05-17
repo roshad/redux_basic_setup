4e4a6c98b96703833c2aab079bd14a00c05e35fe
roshad <roshad87a9@gmail.com>
2021-05-17 21:38:20 +0800

    去掉css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 
 
 import { createStore } from 'redux'
-const store = createStore()
+const store = createStore(()=>{})
 
 ReactDOM.render(
   <React.StrictMode>
